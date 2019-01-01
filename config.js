93f358cb4d991f4a4a61802b0540a50f10320c39
Dameon Mendoza <dameonmendoza@gmail.com>
2019-01-01 15:10:24 -0700

    circleci testing

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   MONGODB_URI:process.env.MONGODB_URI || 'localhost27017',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ,
-  JWT_SECRET : process.env.JWT_SECRET ,
+  JWT_SECRET : process.env.JWT_SECRET || "secret",
   JWT_EXPIRY : process.env.JWT_EXPIRY || '7d',
   
 };
